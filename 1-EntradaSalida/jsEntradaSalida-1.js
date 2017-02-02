bf839757afc13a80dd95ea2a89dfa23aa4f6aa08
mfcespedes <marcoszalazar1109@gmail.com>
2017-02-02 20:23:06 -0300

    Primer Commit
    Hicimos el ejercicio 1 y 2 en clases

--- 1-EntradaSalida/jsEntradaSalida-1.js
+++ 1-EntradaSalida/jsEntradaSalida-1.js
@@ -1,6 +1,11 @@
 //Debemos lograr mostrar un mensaje al presionar el botón  'MOSTRAR'.
 function Mostar()
 {
-	
+	alert("Esto funciona de maravilla");
+	// esto es un comentario de una linea
+	/*
+	esto es un comentario de un bloque
+	esto es mi fuente de datos http://www.w3schools.com/
+	*/
 }
 
--- 1-EntradaSalida/jsEntradaSalida-2.js
+++ 1-EntradaSalida/jsEntradaSalida-2.js
@@ -2,6 +2,15 @@
 y luego mostrarlo por 'alert' al presionar el botón  'MOSTRAR'*/
 function Mostar()
 {
-	
+	var nombre;
+	//nombre = "Marcos";
+	nombre = prompt("Ingresá tu nombre");
+	alert(nombre);
+
+
+
+
+
+	//var person = prompt("Ingresá tu nombre", "Natalia Natalia");
 }
 
