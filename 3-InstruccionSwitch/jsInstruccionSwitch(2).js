ad41e3cc40a6b4c8dd56377947c281825127fc8d
mfcespedes <marcoszalazar1109@gmail.com>
2017-02-10 21:14:10 -0300

    Sexto Commit
    Ejercicios del 1 al 6

--- 3-InstruccionSwitch/jsInstruccionSwitch(2).js
+++ 3-InstruccionSwitch/jsInstruccionSwitch(2).js
@@ -1,9 +1,31 @@
 function Mostrar()
 {
 //tomo la edad  
-var mesDelAño = document.getElementById('mes').value;
+var mesDelAño;
+mesDelAño=document.getElementById('mes').value;
 
-alert (mesDelAño);
+switch(mesDelAño) {
+	case"Junio":
+	case"Julio":
+	case"Agosto":
+		alert("Abrigate que hace frio.");
+		break;
+	case"Enero":
+	case"Febrero":
+	case"Marzo":
+	case"Abril":
+	case"Mayo":
+		alert("Falta para el invierno.");
+		break;
+	case"Septiembre":
+	case"Octubre":
+	case"Noviembre":
+	case"Diciembre":
+		alert("Ya pasamos el frio, ahora calor!!!.");
+		break;
+}
+
+//alert (mesDelAño);
 
 
 
--- 3-InstruccionSwitch/jsInstruccionSwitch(3).js
+++ 3-InstruccionSwitch/jsInstruccionSwitch(3).js
@@ -2,7 +2,15 @@ function Mostrar()
 {
 //tomo la edad  
 var mesDelAño = document.getElementById('mes').value;
+switch (mesDelAño) {
+    case "Febrero":
+    alert("Este mes no tiene más de 29 días");
+    break;
+    default:
+    alert("Este mes tiene 30 o más días");
+    break;
 
+}
 //alert (mesDelAño);
 	
 	
