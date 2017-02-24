0d45d10a0ed30989a8c2f503520d145c6e087951
mfcespedes <marcoszalazar1109@gmail.com>
2017-02-23 22:12:37 -0300

    Decimo Commit
    ejercicios For

--- 6-InstruccionFor/jsIteracionesFor (1).js
+++ 6-InstruccionFor/jsIteracionesFor (1).js
@@ -1,4 +1,71 @@
 function Mostrar()
 {
+	var contador=0;
+	var numeroRandom;
+	var contador1=0;
+	var contador2=0;
+	var contador3=0;
+	var contador4=0;
+	var contador5=0;
+	var contador6=0;
+	var contador7=0;
+	var contador8=0;
+	var contador9=0;
+	var contador10=0;
 
+
+	for(;contador<10000;contador++)
+	{
+		numeroRandom=Math.floor((Math.random() * 10) + 1);
+		switch(numeroRandom)
+		{
+			case 1:
+				contador1++;
+				break;
+			case 2:
+				contador2++;
+				break;
+			case 3:
+				contador3++;
+				break;
+			case 4:
+				contador4++;
+				break;
+			case 5:
+				contador5++;
+				break;
+			case 6:
+				contador6++;
+				break;
+			case 7:
+			if (contador7==7)
+			{
+				continue;
+			}
+				contador7++;
+				break;
+			case 8:
+				contador8++;
+				break;
+			case 9:
+				contador9++;
+				break;
+			case 10:
+				contador10++;
+				break;
+		}
+
+	}
+	console.log("la cantidad de 1 "+contador1*contador/100+"%");
+	console.log("la cantidad de 2 "+contador2*contador/100+"%");
+	console.log("la cantidad de 3 "+contador3*contador/100+"%");
+	console.log("la cantidad de 4 "+contador4*contador/100+"%");
+	console.log("la cantidad de 5 "+contador5*contador/100+"%");
+	console.log("la cantidad de 6 "+contador6*contador/100+"%");
+	console.log("la cantidad de 7 "+contador7*contador/100+"%");
+	console.log("la cantidad de 8 "+contador8*contador/100+"%");
+	console.log("la cantidad de 9 "+contador9*contador/100+"%");
+	console.log("la cantidad de 10 "+contador10*contador/100+"%");
+	
+	
 }
--- 6-InstruccionFor/jsIteracionesFor (2).js
+++ 6-InstruccionFor/jsIteracionesFor (2).js
@@ -1,5 +1,75 @@
 function Mostrar()
 {
+	var mascota;
+	var tienenMascota;
+	var contadorMascotas;
+	var contadorNoMascotas;
+	var tipoMascota;
+	var contador=0;
+	var edad;
+	var nombre;
+	var maximo;
+	var sexo;
+	var nombreMascota;
+	var edadMascota;
+	var edadMaximaMascota;
+	var nombreMascotaMayor;
 
+	for( ;contador<1000; )
+	{
+		
+		nombre=prompt("Ingrese nombre");
+		edad=prompt("Ingrese edad");
+		edad=parseInt(edad);
+		sexo=prompt("Ingrese F o M");
+		while(sexo!="f"&&sexo!="m")
+		{
+			sexo=prompt("Ingrese F o M");
+		}
+		
+		if(contador==1)// falta terminar
+		{
+			maximo=edad;
+			nombre=nombreMayor;
+		}
+		{
+		
 
+		mascota=prompt("Tiene mascotas? Si o No");
+		while(mascota!="si"&&mascota!="no")
+		{
+			mascota=prompt("Tiene mascotas? Si o No");
+		}
+
+		if(mascota="si")
+		{
+			contadorMascotas=contadorMascotas++;
+			while(tipoMascota!="perro"&&tipoMascota!="gato")
+			{
+				tipoMascota=prompt("Ingrese Perro o Gato");
+			}
+
+			nombreMascota=prompt("Ingrese el nombre de la mascota");
+			edadMascota=prompt("Ingrese la edad de la mascota");
+			while(edadMascota<0&&edadMascota>16)
+			{
+				edadMascota=prompt("Ingrese la edad hasta 15 años");
+			}
+
+			if(contador==0)
+			{
+				edadMaximaMascota=edadMascota;
+				nombreMascotaMayor=nombreMascota;
+			}
+		else
+		{
+			contadorNoMascotas=contadorNoMascotas++;//edad mas viejo del que no tiene mascota
+			
+		}
+		tipoMascota=prompt("Ingrese si es Perro o Gato");
+
+		
+		contador++;
+		// necesito saber el nombre del primer gato que sea de una mujer mayor a 30 años y el gato no tenga mas de 5 años de edad
+	}
 }
